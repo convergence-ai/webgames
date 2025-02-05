6e02f9473cd9319f771f28aa4154f286e348c2ee
RobbenRibery <runodng.liu@outlook.com>
2025-02-05 20:47:24 +0000

    challenge 52 modify | remove print

--- webgames/src/pages/VerifyChallenge.tsx
+++ webgames/src/pages/VerifyChallenge.tsx
@@ -24,7 +24,6 @@ export default function LoginChallenge() {
   };
 
   const [correctCode] = useState(generateVerificationCode());
-  console.log(correctCode);
 
   useEffect(() => {
     const canvas = canvasRef.current;
